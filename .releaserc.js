3b1d6e3e28b36087acd7e84e03485675e1e2a85b
Manuel Bujes Garrido <manu.bujes@inari.io>
2023-09-29 21:42:47 +0200

    fix: changelog path

--- .releaserc.js
+++ .releaserc.js
@@ -14,7 +14,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/**/*.{js,mjs,cjs,css}", "docs", "package.json"],
+        assets: ["dist/**/*.{js,mjs,cjs,css}", "CHANGELOG.md", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
