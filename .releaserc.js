b5e8e8efb3447cc42d3ea581548b7f791d54a700
Manuel Bujes Garrido <manu.bujes@inari.io>
2023-09-29 20:33:23 +0200

    ci: add all file builds

--- .releaserc.js
+++ .releaserc.js
@@ -14,7 +14,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/**/*.{js,css}", "docs", "package.json"],
+        assets: ["dist/**/*.{js,mjs,cjs,css}", "docs", "package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
